ac0ea9983295273d15850145a525cbba5a293d7c
Rayhansajal <rayhansajal1996@gmail.com>
2026-02-20 21:40:51 +0600

    User entity,dto and reposity

---
+++ src/main/java/com/example/FoodApp/auth_users/dto/RegistrationRequest.java
@@ -0,0 +1,27 @@
+package com.example.FoodApp.auth_users.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+
+public class RegistrationRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+    @NotBlank(message = "Email is required")
+    @Size(min = 3,message = "Password must be 3 characters long")
+    private String password;
+    @NotBlank(message = "Address is required")
+    private String address;
+    @NotBlank(message = "PhoneNumber is required")
+    private String phoneNumber;
+    private List<String> roles;
+
+}
---
+++ src/main/java/com/example/FoodApp/auth_users/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.example.FoodApp.auth_users.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String profileUrl;
+    private String phoneNumber;
+
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private String address;
+    private List<RoleDTO> roles;
+
+    private boolean isActive;
+    private MultipartFile imageFile;
+}
