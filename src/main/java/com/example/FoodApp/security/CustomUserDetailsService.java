fc81c6045f5b3ae11cf816ed0fde639c5561e305
Rayhansajal <rayhansajal1996@gmail.com>
2026-03-03 16:49:22 +0600

    Add Security

---
+++ src/main/java/com/example/FoodApp/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.FoodApp.security;
+
+import com.example.FoodApp.auth_users.entity.User;
+import com.example.FoodApp.auth_users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
